91231d290e060cc5dca6575ca3b7bad128606318
xjzsq <sdxjzsq@126.com>
2020-02-27 22:28:17 +0800

    wu han jia you!

---
+++ 2019-nCoV.h
@@ -0,0 +1,48 @@
+#include<cstdio>
+#include<string>
+#include<map>
+#include<stack>
+#include<vector>
+#include<iostream> 
+using namespace std;
+const int maxn = 2020112;
+#define peace 1
+#define conscience int conscience = 1
+#define man man = China::_2019_nCoV = 1
+map<string,int> gz;
+#define gengzi gz
+struct War
+{
+	bool smoke,on;
+	int time;
+	string enemy;
+}war;
+struct Door
+{
+	void knock(int year)
+	{
+		printf("\"战疫\"前线\n时间是生命\n\"逆行者\"是勇士\n眼泪与坚强\n团结与信仰\n构筑起\n抵抗疫情的\n\"雷火神山\"\n");
+	}
+}*door;
+
+stack<int> rumors;
+void spread(int message)
+{
+	war.on=false;
+	//
+}
+#define people int
+namespace China 
+{
+	long long moon=0,mask=2147483647,_2019_nCoV=0;
+	bool mask_enough(long long mask_count)
+	{
+		return false;
+	}
+}
+namespace Japan 
+{
+	long long moon=0,mask=2147483647;
+}
+int outdoor=1000,message=1,rumor=0,all_power=2147483647;
+vector<int> anti_virus;
