9ad5d4981562d5a9f852111d9c754a3e22b988ce
xjzsq <sdxjzsq@126.com>
2020-02-27 22:28:17 +0800

    wu han jia you!

---
+++ anti-virus.cpp
@@ -0,0 +1,24 @@
+#include"2019-nCoV.h"
+int main(){
+	war.smoke = false,war.on = true;
+	door->knock(2020);
+	war.enemy = "2019-nCoV";
+	while(1){if(war.on)break;gengzi["status"] = peace;}
+	
+	do
+	{
+		outdoor--;
+		message==rumor?rumors.push(message):spread(message);
+		const conscience;
+		anti_virus.push_back(all_power);
+	}while(war.on);
+
+	China::moon==Japan::moon;
+	if(!China::mask_enough(China::mask))China::mask+=Japan::mask;
+	while(China::_2019_nCoV==0){
+		union{people man;people woman;}CHN;
+	}
+
+	cout << "武汉加油！"<<endl;
+	cout << "中国加油！"<<endl;
+}
